fc453b2658deae95e4b38a780d16716ed7dd4790
gunkarlsson <hi@gunkarlsson.com>
2021-05-10 12:03:06 +0200

    ⚰️  labb 01

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 //-------------------------------- LABB 1 --------------------------------
-
 //Require ("importing") express and create a webserver that we connect to app
 const express = require("express");
 const app = express();
@@ -13,7 +12,7 @@ const path = require("path");
 //To enable serving static files. Here we serve all files in the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
-//------ 1A -------
+//---------------- 1A ---------------
 //Get a random number between 0 and 1023
 app.get("/api/random", (req, res) => {
   const random = Math.floor(Math.random() * 1023);
@@ -21,7 +20,7 @@ app.get("/api/random", (req, res) => {
   //we return a json-file immediately instead of using content-type etc
 });
 
-//------ 1B -------
+//---------------- 1B ---------------
 //Write a number of your choice in the URL and receive a random number between 0 and your number
 app.get("/api/custom_random/:num", (req, res) => {
   //Access the URL param value
@@ -31,7 +30,7 @@ app.get("/api/custom_random/:num", (req, res) => {
   res.json({ number: customRandom });
 });
 
-//------ 1C -------
+//---------------- 1C ---------------
 app.post("/api/:word", (req, res) => {
   // Access the word from params
   const word = req.params.word;
@@ -45,79 +44,13 @@ app.post("/api/:word", (req, res) => {
   });
 });
 
-//-------------------------------- LABB 2 --------------------------------
-//Import filesystem
-const fs = require("fs");
-
-//------ 2A -------
-//Get counter data
-app.get("/api/counter", (req, res) => {
-  // Check data is inside the counter textfile
-  fs.readFile("./db/counter.txt", (err, data) => {
-    // Check for errors
-    if (err) {
-      console.log(err);
-    }
-    // Turn content of counter.txt into a string
-    let countNum = data.toString();
-    // send back counters current state as the respons in a json object
-    res.json({ counter: countNum });
-  });
-});
-
-//------ 2B -------
-// Add one to counter
-//Put replaces/updates the resource
-app.put("/api/add", (req, res) => {
-  // Check which data in counter text file ( i.e current counter state)
-  fs.readFile("./db/counter.txt", (err, data) => {
-    // If error, log to console
-    if (err) {
-      console.log(err);
-    }
-    // 1. Turn content of counter.txt into a number and add 1
-    // 2. Turn it a string & put it in a new variable
-    addOne = (Number(data) + 1).toString();
-    console.log(
-      `You have added 1 to the counter. The new number is: ${addOne}`
-    );
-    // Update the state of counter by adding 1 to it
-    fs.writeFile("./db/counter.txt", addOne, () => {
-      // send back addOne as the response in a json object
-      res.json({ counter: addOne });
-    });
-  });
-});
-
-app.get("/api/subtract", (req, res) => {
-  // Check what data is inside the counter text file (our current counter state)
-  fs.readFile("./db/counter.txt", (err, data) => {
-    // Check for errors
-    if (err) {
-      console.log(err);
-    }
-
-    // 1. Turn content of counter.txt into a number and subtract 1
-    // 2. Turn it a string & put it in a new variable
-    subtractOne = (Number(data) - 1).toString();
-    console.log(`countNum is now ${subtractOne}`);
-
-    // Update the state of counter by adding 1 to it
-    fs.writeFile("./db/counter.txt", subtractOne, () => {
-      // send back countNum as the respons in a json object
-      res.json({ counter: subtractOne });
-    });
-  });
-});
-
 app.listen(PORT, () => {
   console.log(`Now we're listening at http://localhost:${PORT}`, __dirname);
 });
 
 // MISC
-// req = information about the request, res = the response that's sent back
 // .USE, .GET etc are express syntax
 // after a successful .GET, the following code will not run, it stops after the GET request.
 // unlike .GET, .USE will always run
 // PUT: calling the same PUT request multiple times will always produce the same result
-//POST: calling a POST request repeatedly have side effects of creating the same resource multiple times.
+// POST: calling a POST request repeatedly have side effects of creating the same resource multiple times.
