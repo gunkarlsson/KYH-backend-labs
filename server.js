021680833f142fa070ea19d2b0bedc99062c0cf0
gunkarlsson <hi@gunkarlsson.com>
2021-05-17 12:05:09 +0200

    🚀  labb 03

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-//-------------------------------- LABB 2 --------------------------------
-//Require ("importing") express and create a webserver that we connect to app
+//-------------------------------- LABB 3 --------------------------------
 const express = require("express");
 const app = express();
-//Import filesystem
 const fs = require("fs");
-//Port as a variable, set to 5000
 const PORT = 5000;
-
-//Importing/declaring path
 const path = require("path");
 
-//To enable serving static files. Here we serve all files in the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
+
+//---------------- 1A ---------------
+app.get("/api/random", (req, res) => {
+  const random = Math.floor(Math.random() * 1023);
+  res.json({ status: "success", number: random });
+});
 
 //---------------- 2A ---------------
 //Get counter data
@@ -25,49 +25,46 @@ app.get("/api/counter", (req, res) => {
     // Turn content of counter.txt into a string
     let countNum = data.toString();
     // Send back counter's current state as the response in a json object
-    res.json({ counter: countNum });
+    res.json({ status: "success", counter: countNum });
   });
 });
 
 //---------------- 2B ---------------
-// Add one to counter
-//Put replaces/updates the resource
-app.put("/api/add", (req, res) => {
-  // Check which data in counter text file ( i.e current counter state)
+app.get("/api/add", (req, res) => {
   fs.readFile("./db/counter.txt", (err, data) => {
-    // If error, log to console
     if (err) {
       console.log(err);
     }
-    // 1. Turn content of counter.txt into a number and add 1
-    // 2. Turn it a string & put it in a new variable
+    countBeforeAdd = Number(data).toString();
     addOne = (Number(data) + 1).toString();
     console.log(
-      `You have added 1 to the counter. The new number is: ${addOne}`
+      `You have added 1 to the counter. It was ${countBeforeAdd}, and the new number is: ${addOne}`
     );
-    // Update the state of counter by adding 1 to it
     fs.writeFile("./db/counter.txt", addOne, () => {
-      // Send back addOne as the response in a json object
-      res.json({ counter: addOne });
+      res.json({
+        countBeforeAdd: Number(countBeforeAdd),
+        counter: Number(addOne),
+      });
     });
   });
 });
 
+//---------------- 2C ---------------
 app.get("/api/subtract", (req, res) => {
-  // Check what data is inside the counter text file (our current counter state)
   fs.readFile("./db/counter.txt", (err, data) => {
-    // Check for errors
     if (err) {
       console.log(err);
     }
-    // 1. Turn content of counter.txt into a number and subtract 1
-    // 2. Turn it a string & put it in a new variable
+    countBeforeSubtract = Number(data).toString();
     subtractOne = (Number(data) - 1).toString();
-    console.log(`countNum is now ${subtractOne}`);
-    // Update the state of counter by subtracting 1 from it
+    console.log(
+      `It was ${countBeforeSubtract}, countNum is now ${subtractOne}`
+    );
     fs.writeFile("./db/counter.txt", subtractOne, () => {
-      // send back countNum as the respons in a json object
-      res.json({ counter: subtractOne });
+      res.json({
+        countBeforeSubtract: Number(countBeforeSubtract),
+        counter: Number(subtractOne),
+      });
     });
   });
 });
@@ -77,6 +74,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-// MISC
-// PUT: calling the same PUT request multiple times will always produce the same result
-//POST: calling a POST request repeatedly have side effects of creating the same resource multiple times.
---
+++ test/app.test.js
@@ -0,0 +1,83 @@
+const app = require("../server");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { expect } = chai;
+
+chai.use(chaiHttp);
+describe("/api/random should return a random number between 0-1023", () => {
+  it("to return a random number between 0-1023 and status to be success", (done) => {
+    chai
+      .request(app)
+      .get("/api/random")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals("success");
+        expect(res.body.number).to.be.a("number");
+        done();
+      });
+  });
+});
+
+describe("/api/counter should be a string", () => {
+  it("to return the number in counter and status to be success", (done) => {
+    chai
+      .request(app)
+      .get("/api/counter")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals("success");
+        expect(res.body.counter).to.be.a("string");
+        done();
+      });
+  });
+});
+
+describe("/api/add parameters should be numbers", () => {
+  it("the parameters should be numbers", (done) => {
+    chai
+      .request(app)
+      .get("/api/add")
+      .end((err, res) => {
+        expect(res.body.countBeforeAdd).to.be.a("number");
+        expect(res.body.counter).to.be.a("number");
+        done();
+      });
+  });
+});
+
+describe("/api/add should add +1 to the counter", () => {
+  it("to add one to the number in counter ", (done) => {
+    chai
+      .request(app)
+      .get("/api/add")
+      .end((err, res) => {
+        expect(res.body.counter).to.equals(res.body.countBeforeAdd + 1);
+        done();
+      });
+  });
+});
+
+describe("/api/subtract parameters should be numbers", () => {
+  it("the parameters should be numbers", (done) => {
+    chai
+      .request(app)
+      .get("/api/subtract")
+      .end((err, res) => {
+        expect(res.body.countBeforeSubtract).to.be.a("number");
+        expect(res.body.counter).to.be.a("number");
+        done();
+      });
+  });
+});
+
+describe("/api/subtract should subtract -1 to the counter", () => {
+  it("to subtract one to the number in counter ", (done) => {
+    chai
+      .request(app)
+      .get("/api/subtract")
+      .end((err, res) => {
+        expect(res.body.counter).to.equals(res.body.countBeforeSubtract - 1);
+        done();
+      });
+  });
+});
